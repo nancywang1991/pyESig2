63312e117a38f2fe7bf2a5d22b2e6d722f3303aa
Nancy Wang <wangnxr@cs.washington.edu>
2016-02-25 15:42:52 -0700

    added flic_data format extractions

--- vid/flic_data_add.py
+++ vid/flic_data_add.py
@@ -69,36 +69,36 @@ for sbj in subjects:
         crop = label_crop(f)
         if crop is not None:
             #poselet_hit_idx
-            new_example[0]=[np.NAN]
+            new_example[0][0]=[np.NAN]
             #moviename
-            new_example[1]=[sbj]
+            new_example[0][1]=[sbj]
             #coords
-            new_example[2]=label_joints(f)
+            new_example[0][2]=label_joints(f)
             img_name = f.split("/")[-1][:-6] + ".jpg"
             img_path = image_path_main + sbj + "/JPEGImages/" + img_name
             #filepath
-            new_example[3]=img_path
+            new_example[0][3]=img_path
             img_dim = Image.open(img_path).size
             #imgdims
-            new_example[4]=[img_dim[0], img_dim[1], 3]
+            new_example[0][4]=[img_dim[0], img_dim[1], 3]
             frame = int(img_name.split("_")[-1][:-4])
             #currframe
-            new_example[5]=[[frame]]
+            new_example[0][5]=[[frame]]
             #torsobox
-            new_example[6]=crop
+            new_example[0][6]=crop
             if np.random.randint(0,100)>75:
                 #istrain
-                new_example[7]=[[0]]
+                new_example[0][7]=[[0]]
                 #istest
-                new_example[8]=[[1]]
+                new_example[0][8]=[[1]]
             else:
                 np.append(tr_indices["tr_plus_indices"],np.array([[len(examples['examples'][0])+1]]), axis=0)
-                new_example[7]=[[1]]
-                new_example[8]=[[0]]
+                new_example[0][7]=[[1]]
+                new_example[0][8]=[[0]]
             #isbad
-            new_example[9]=[[0]]
+            new_example[0][9]=[[0]]
             #isunchecked
-            new_example[10]=[[0]]
+            new_example[0][10]=[[0]]
             np.append(examples['examples'][0], new_example, axis=0)
             pdb.set_trace()
 
