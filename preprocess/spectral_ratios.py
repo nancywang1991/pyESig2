df59c6488e8c1c2a5d760807d6997d15e664238f
Nancy Wang <wangnxr@cs.washington.edu>
2016-07-29 16:34:28 -0700

    added spectra ratio pipeline and improved figures

--- preprocess/spectral_ratios.py
+++ preprocess/spectral_ratios.py
@@ -49,7 +49,6 @@ def plot_2d_coords(result, ratio1, ratio2):
 
     for h in range(0,21,5):
         t = h/5
-        pdb.set_trace()
         samples = np.random.choice(len(result[t*60*60:(t+1)*60*60,0]), 0.1*len(result[t*60*60:(t+1)*60*60,0]))
         axes[t].scatter(result[samples,0], result[samples,1], s=0.2, c="black", edgecolors="face")
         axes[t].set_title("%i o'clock" % ((h+8)%24))
