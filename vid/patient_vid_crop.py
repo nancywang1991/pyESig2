6e3d6f93fee3457b3b0d150841bd79058df34ab0
Nancy Wang <wangnxr@cs.washington.edu>
2016-08-16 13:47:07 -0700

    improved pose_detection pipeline

--- vid/patient_vid_crop.py
+++ vid/patient_vid_crop.py
@@ -11,7 +11,7 @@ def gen_cropped_frames(video_path, coords_path, save_path):
     fnum = fname.split("_")[-1]
     frame_count = 0
     coords = open(coords_path).readlines()
-    if cap.get("CV_CAP_PROP_FPS") < 29:
+    if cap.get(cv2.cv.CV_CAP_PROP_FPS) < 29:
         append_frame = int(30/(30-cap.get(cv2.cv.CV_CAP_PROP_FPS)))
         for i in xrange(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)/append_frame):
             coords.insert(i*append_frame, coords[i*append_frame])
