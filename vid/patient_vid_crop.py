e611a7ef7ef5627ea008a67d7e7b6f8eeb0fa73e
Nancy Wang <wangnxr@cs.washington.edu>
2016-08-16 14:05:41 -0700

    improved pose_detection pipeline

--- vid/patient_vid_crop.py
+++ vid/patient_vid_crop.py
@@ -22,7 +22,9 @@ def gen_cropped_frames(video_path, coords_path, save_path):
     output_vid = my_video_capture(save_path, frame_rate=30, mode="write")
 
     if int(fnum) > 0:
-        prev_fname = "_".join(fname.split("_")[:-1].append(str(int(fnum)-1)))
+        prev_fname = fname.split("_")[:-1]
+        prev_fname.append(str(int(fnum)-1))
+        prev_fname = "_".join(prev_fname)
         use_coord = np.array([int(n) for n in open("%s/%s/crop_coords.txt" % (save_path, prev_fname)).readlines()[-1].split(",")])
     while vid.has_next():
         frame = vid.read()
