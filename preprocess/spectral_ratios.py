c9020e4bf5095d2d4170d3d5cb4be20fadf0b720
Nancy Wang <wangnxr@cs.washington.edu>
2016-08-19 13:57:50 -0700

    improved pose_detection pipeline

--- preprocess/spectral_ratios.py
+++ preprocess/spectral_ratios.py
@@ -59,7 +59,7 @@ def plot_2d_coords(result, ratio1, ratio2, save_fldr, day):
         plt.tight_layout()
         f.savefig("%s/figures/%i_%04d.jpg" % (save_fldr, day, count))
         count += 1
-        f.close()
+        plt.close()
 
 def plot_time(result, c):
     f = plt.figure()
