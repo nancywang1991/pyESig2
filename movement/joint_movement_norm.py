b13154af37cb652893abf9a7444e92ad8c186e31
Nancy Wang <wangnxr@cs.washington.edu>
2016-08-29 13:20:51 -0700

    improved pose_detection pipeline

--- movement/joint_movement_norm.py
+++ movement/joint_movement_norm.py
@@ -97,7 +97,7 @@ def main(args):
             os.makedirs('%s/poses_%i/' % (args.save, itr))
         for r, row in enumerate(prev_poses[1:]):
             pdb.set_trace()
-            img_pred = draw_joints(prev_frame, prev_poses[r+1], True, 1)
+            img_pred = draw_joints(cv2.imread("%s/%05i.png" % (args.datadir, r+1)), prev_poses[r+1], True, 1)
             cv2.imwrite(img_pred, '%s/poses_%i/%05i.png' % (args.save, itr, r+1))
             print r
             frame = cv2.cvtColor(cv2.imread("%s/%05i.png" % (args.datadir, r+2)),  cv2.COLOR_BGR2GRAY)     
