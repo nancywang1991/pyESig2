e57fb62191c9c8474d8d575119f6ecf779eff7e1
Nancy Wang <wangnxr@cs.washington.edu>
2016-08-29 14:21:35 -0700

    improved pose_detection pipeline

--- movement/joint_movement_norm.py
+++ movement/joint_movement_norm.py
@@ -71,8 +71,10 @@ def optical_flow_mvmt(frame, prev_frame, pose_pos):
          pdb.set_trace()
 
     optical_pos = []
+    p0 = np.array([p[0] for p in p0])
+    p1 = np.array([p[0] for p in p1])
     for pos in pose_pos:
-        p0 = np.array([p[0] for p in p0])
+
         point_dist = np.array([np.abs(pos[0]-p[0]) + np.abs(pos[1]-p[1]) for p in p0])
         pdb.set_trace()
         nearby_points = np.where(point_dist < 30)[0]
