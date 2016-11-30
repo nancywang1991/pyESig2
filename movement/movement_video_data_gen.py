97b2aa008d66a9bf2e8d6a49ae0179b35087abba
Nancy Wang <nancywang1991@yahoo.ca>
2016-11-29 18:38:06 -0800

    joint movement updates

--- movement/movement_video_data_gen.py
+++ movement/movement_video_data_gen.py
@@ -43,20 +43,24 @@ def main(mv_file, vid_file, save_dir):
         os.makedirs(args.save_dir + "/l_arm_0")
         os.makedirs(args.save_dir + "/l_arm_1")
 
-    for f in range(15,len(mv_file), 5):
+    for f in range(15,len(mv_file)):
         img = vid_file.read()
         if np.all(left_arm_mvmt[f:f+5]>2):
             cv2.imwrite("%s/l_arm_1/%s_%i.png" %(save_dir, vid_name, f-15), img)
-        elif np.all(left_arm_mvmt[f:f+5]>=0) and np.all(left_arm_mvmt[f:f+5]<0.1):
-            cv2.imwrite("%s/l_arm_0/%s_%i.png" % (save_dir, vid_name, f-15), img)
         if np.all(right_arm_mvmt[f:f+5]>2):
             cv2.imwrite("%s/r_arm_1/%s_%i.png" %(save_dir, vid_name, f-15), img)
-        elif np.all(right_arm_mvmt[f:f+5]>=0) and np.all(right_arm_mvmt[f:f+5]<0.1):
-            cv2.imwrite("%s/r_arm_0/%s_%i.png" % (save_dir, vid_name, f-15), img)
         if np.all(head_mvmt[f:f+5]>1):
             cv2.imwrite("%s/head_1/%s_%i.png" %(save_dir, vid_name, f-15), img)
-        elif np.all(head_mvmt[f:f+5]>=0) and np.all(head_mvmt[f:f+5]<0.1):
-            cv2.imwrite("%s/head_0/%s_%i.png" % (save_dir, vid_name, f-15), img)
+    vid_file.rewind()
+    for f in range(15, len(mv_file), 60):
+        vid_file.forward_to(f-15)
+        img = vid_file.read()
+        if np.all(left_arm_mvmt[f:f + 5] >= 0) and np.all(left_arm_mvmt[f:f + 5] < 0.1):
+            cv2.imwrite("%s/l_arm_0/%s_%i.png" % (save_dir, vid_name, f - 15), img)
+        if np.all(right_arm_mvmt[f:f + 5] >= 0) and np.all(right_arm_mvmt[f:f + 5] < 0.1):
+            cv2.imwrite("%s/r_arm_0/%s_%i.png" % (save_dir, vid_name, f - 15), img)
+        if np.all(head_mvmt[f:f + 5] >= 0) and np.all(head_mvmt[f:f + 5] < 0.1):
+            cv2.imwrite("%s/head_0/%s_%i.png" % (save_dir, vid_name, f - 15), img)
 
 
 if __name__ == "__main__":
