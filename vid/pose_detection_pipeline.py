334a286c1c6b05d727351fea919121fcbcf605bf
Ubuntu <wangnxr@gpueast000.r0wy41wnrgdulcdoutiescz5aa.bx.internal.cloudapp.net>
2017-01-18 00:05:17 +0000

    some bugs

--- vid/pose_detection_pipeline.py
+++ vid/pose_detection_pipeline.py
@@ -8,23 +8,23 @@ from pyESig2.vid.pose_detection_preprocessing_pipeline_secure import main as pat
 import getpass
 
 def main(args, password):
-    print "Processing %s" % os.path.basename(args.save)
-    if not os.path.exists(args.save):
-        os.makedirs(args.save)
+    print "Processing %s" % os.path.basename(args.save_dir)
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
     if not os.path.exists(args.s_temp):
         os.makedirs(args.s_temp)
     patient_extract(args, password)
     os.chdir(args.deep_home)
-    subprocess.call("python python/pose/demo_secure.py -v %s -s %s -gpu %i" % (args.s_temp, args.save, args.gpu_id), shell=True)
-    subprocess.call("scp -r %s wangnxr@visiongpu.cs.washington.edu:/mnt/transferred_results/pose/" % (args.save), shell=True)
-    subprocess.call("scp -r %s/crop_coords wangnxr@visiongpu.cs.washington.edu:/mnt/transferred_results/pose/%s/" % (args.s_temp, os.path.basename(args.save)), shell=True)
+    subprocess.call("python python/pose/demo_secure.py -v %s -s %s -gpu %i -pass %s" % (args.s_temp, args.save_dir, args.gpu_id, password), shell=True)
+    subprocess.call("scp -r %s wangnxr@visiongpu.cs.washington.edu:/mnt/transferred_results/pose/" % (args.save_dir), shell=True)
+    subprocess.call("scp -r %s/crop_coords wangnxr@visiongpu.cs.washington.edu:/mnt/transferred_results/pose/%s/" % (args.s_temp, os.path.basename(args.save_dir)), shell=True)
 
 if __name__== "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-home', '--home', help="Home", default="/home/wangnxr/Documents/")
     parser.add_argument('-d', '--dir', required=True, help="Video directory")
     parser.add_argument('-s_temp', '--s_temp', required=True, help="Patient Extracted Video Save Directory")
-    parser.add_argument('-s', '--save', required=True, help="Save Directory" )
+    parser.add_argument('-s', '--save_dir', required=True, help="Save Directory" )
     parser.add_argument('-dark', '--dark_home', default='/home/wangnxr/Documents/darknet/', help='Darknet home' )
     parser.add_argument('-deep', '--deep_home', default='/home/wangnxr/Documents/caffe-heatmap/', help='Caffe-heatmap home')
     parser.add_argument('-w', '--yolo_weights',
