a64eb3f01c8fdbbbcbd103d383753f1cb017b8e3
Ubuntu <wangnxr@gpueast000.r0wy41wnrgdulcdoutiescz5aa.bx.internal.cloudapp.net>
2017-01-21 04:12:42 +0000

    download fix

--- util/encrypt_files.py
+++ util/encrypt_files.py
@@ -10,21 +10,26 @@ if __name__ == "__main__":
     parser.add_argument('-st', '--step', type=int, default=0, help="step to start at")
     args = parser.parse_args()
     password = getpass.getpass()
+    parent = os.path.dirname(args.file)
+    filename=os.path.basename(args.file).split("_")[0]
     if (args.step<=0):
-    	parent = os.path.dirname(args.file)
     	subprocess.call("openssl enc -d -des -in %s -out %s -pass pass:%s" % (args.file, args.file.split(".")[0] + ".tar", password), shell=True)
     	os.remove(args.file)
     if args.step<=1:
         #pdb.set_trace()
-    	subprocess.call("tar -xvzf %s.tar" % args.file, shell=True)
+    	subprocess.call("tar -xvzf %s.tar -C /%s/" % (args.file, parent), shell=True)
     	os.remove("%s.tar" % args.file.split(".")[0])
     if args.step<=2:
-    	new_folder = glob.glob("%s/*/*/*/*/*/*/*/*" % parent)[0]
-    	subprocess.call("mv %s %s" %("/".join(new_folder.split("/")[:-2]), parent), shell=True)
+    	new_folder = glob.glob("%s/*/*/*/*/*/%s/*/*" % (parent, filename))
+        #pdb.set_trace()
+        if len(new_folder) == 0:
+	    new_folder = glob.glob("%s/*/*/*/*/*/*/%s/*/*" % (parent, filename))
+        new_folder = new_folder[0]
+    	subprocess.call("mv %s %s/%s" %("/".join(new_folder.split("/")[:-2]), parent, filename), shell=True)
     if args.step<=3:
-    	for file in glob.glob(parent + "/*/*/*.avi"):
+    	for file in glob.glob(parent + "/%s/*/*.avi" % filename):
         	subprocess.call("openssl enc -e -des -in %s -out %s -pass pass:%s" % (file, file + ".enc", password), shell=True)
     if args.step<=4:
-	for file in glob.glob(parent + "/*/*/*"):
+	for file in glob.glob(parent + "/%s/*/*" % filename):
             if not file.split(".")[-1] == "enc" and not os.path.isdir(file):
                 os.remove(file)
