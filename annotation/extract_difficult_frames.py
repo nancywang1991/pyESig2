e2605a0b5acb21ff6b7b70ddd93840b8aa516b06
Nancy Wang <nancywang1991@yahoo.ca>
2017-01-23 18:41:40 -0800

    wrote pose detection full pipeline on cluster

--- annotation/extract_difficult_frames.py
+++ annotation/extract_difficult_frames.py
@@ -12,7 +12,7 @@ def main(joints_file, vid_file, save_dir):
     downsample_fact = 6
     difficult_frames = []
     for p, pose in enumerate(poses):
-        if p%downsample_fact==0 and ((p-difficult_frames[-1])>30 or len(difficult_frames)==0) and np.any(pose[:,2]<0.1):
+        if p%downsample_fact==0 and (len(difficult_frames)==0 or (p-difficult_frames[-1])>30 ) and np.any(pose[:,2]<0.1):
             difficult_frames.append(p)
 
     vid_name = os.path.split(vid_file)[-1].split('.')[0]
