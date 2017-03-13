2c20beb03da5fbb97df1dc6c1e7c49e8ad72d413
Nancy Wang <nancywang1991@yahoo.ca>
2017-03-13 13:56:43 -0700

    mirror files update to multiple frame copies

--- util/standardize_dataset.py
+++ util/standardize_dataset.py
@@ -18,29 +18,32 @@ def main(dir, test_day):
         else:
             if not os.path.exists("%s/test/%s" % (dir, subdir)):
                 os.makedirs("%s/test/%s" %  (dir, subdir))
-            subprocess.call("mv %s/train/%s/*_%i_* %s/test/%s/" % (dir, subdir, test_day, dir, subdir), shell=True)
+                subprocess.call("mv %s/train/%s/*_%i_* %s/test/%s/" % (dir, subdir, test_day, dir, subdir), shell=True)
 
             if not os.path.exists("%s/val/%s" % (dir, subdir)):
                 os.makedirs("%s/val/%s" % (dir, subdir))
-            subprocess.call("mv %s/train/%s/*_*_*[5-9]9_ %s/val/%s/" % (dir, subdir, dir, subdir), shell=True)
+                subprocess.call("mv %s/train/%s/*_*_*[5-9]9_ %s/val/%s/" % (dir, subdir, dir, subdir), shell=True)
     # Balance test set
     min_set_size = min([len(glob.glob("%s/test/%s/*" % (dir,subdir))) for subdir in os.listdir(dir + "/test/")])
     for subdir in os.listdir(dir + "/test/"):
         to_remove = len(glob.glob("%s/test/%s/*" % (dir,subdir))) - min_set_size
-        to_remove_list = np.random.shuffle(glob.glob("%s/test/%s/*" % (dir,subdir)))[:to_remove]
-        for file in to_remove_list:
+        to_remove_list = glob.glob("%s/test/%s/*" % (dir,subdir))
+        np.random.shuffle(to_remove_list)
+        for file in to_remove_list[:to_remove]:
             os.remove(file)
     # Balance val set
     min_set_size = min([len(glob.glob("%s/val/%s/*" % (dir, subdir))) for subdir in os.listdir(dir + "/val/")])
     for subdir in os.listdir(dir + "/val/"):
         to_remove = len(glob.glob("%s/val/%s/*" % (dir, subdir))) - min_set_size
-        to_remove_list = np.random.shuffle(glob.glob("%s/val/%s/*" % (dir, subdir)))[:to_remove]
-        for file in to_remove_list:
+        to_remove_list = glob.glob("%s/val/%s/*" % (dir, subdir))
+        np.random.shuffle(to_remove_list)
+        for file in to_remove_list[:to_remove]:
             os.remove(file)
     # Make train set total size be a power of 24
     to_remove = len(glob.glob("%s/train/%s/*" % (dir, subdir)))%24
-    to_remove_list = np.random.shuffle(glob.glob("%s/train/mv_0/*" % (dir)))[:to_remove]
-    for file in to_remove_list:
+    to_remove_list = glob.glob("%s/train/mv_0/*" % (dir))
+    np.random.shuffle(to_remove_list)
+    for file in to_remove_list[:to_remove]:
         os.remove(file)
 
 if __name__=="__main__":
