7ce631fbd03388382e58e1ee4571fda02fef68d8
nancywang1991 <nancywang1991@yahoo.ca>
2017-04-18 16:52:08 -0700

    file decryption

---
+++ util/decrypt_files.py
@@ -0,0 +1,30 @@
+import argparse
+import getpass
+import subprocess
+import os
+import glob
+import pdb
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', required=True, help="original encrypted file")
+    parser.add_argument('-st', '--step', type=int, default=0, help="step to start at")
+    args = parser.parse_args()
+    password = getpass.getpass()
+    parent = os.path.dirname(args.file)
+    filename=os.path.basename(args.file).split("_")[0]
+    if (args.step<=0):
+    	subprocess.call("openssl enc -d -des -in %s -out %s -pass pass:%s -nopad" % (args.file, args.file.split(".")[0] + ".tar", password), shell=True)
+    	#os.remove(args.file)
+    if args.step<=1:
+        #pdb.set_trace()
+    	subprocess.call("tar -xvzf %s.tar " % (args.file), shell=True)
+    	#os.remove("%s.tar" % args.file.split(".")[0])
+    if args.step<=2:
+    	new_folder = glob.glob("%s/*/*/*/*/*/*/%s/*/*" % (parent, filename))
+        #pdb.set_trace()
+        if len(new_folder) == 0:
+	    new_folder = glob.glob("%s/*/*/*/*/*/*/*/%s/*/*" % (parent, filename))
+        if len(new_folder) == 0:
+            new_folder = glob.glob("%s/*/*/*/*/*/%s/*/*" % (parent, filename))
+	new_folder = new_folder[0]
+    	subprocess.call("mv %s %s/" %("/".join(new_folder.split("/")[:-2]), parent), shell=True)
