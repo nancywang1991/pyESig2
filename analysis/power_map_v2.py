d0dce3bd02ccb97de73aaf1d21c3b12eb1a9e340
Nancy Wang <nancywang1991@yahoo.ca>
2017-06-16 15:37:14 -0700

    mirror files update to multiple frame copies

--- analysis/power_map_v2.py
+++ analysis/power_map_v2.py
@@ -7,29 +7,66 @@ import numpy as np
 import scipy.io
 import pdb
 import scipy.stats
+import matplotlib.pyplot as plt
+from scipy import signal
 
 def main(ecog_loc, f_min, f_max):
 
-    tracks = {"r_arm_1": [], "mv_0": [], "l_arm_1": [], "head_1": []}
+    tracks = {"r_arm_1": [], "mv_0": []}
     tracks_final = {}
     significance = {}
+
+
     for track in tracks.iterkeys():
+        displaymat = np.zeros(shape=(64, 129,85))
         print "Working on %s" % track
         for f, file in enumerate(sorted(glob.glob(ecog_loc + "/%s/*.npy" % track))):
             ecog = np.load(file)
-            frequency = np.zeros(shape=(64, f_max-f_min))
+            for c in range(64):
+                f, t, Sxx = signal.spectrogram(ecog[c], 1000, noverlap=200)
+                displaymat[c]+=Sxx
+
+
+            frequency = np.zeros(shape=(64, (f_max-f_min)/2))
             for c in xrange(64):
-                frequency[c, :] = np.abs(np.fft.fft(ecog[c,-1500:-500]) ** 2)[f_min:f_max] -np.abs(np.fft.fft(ecog[c,-5000:-4000]) ** 2)[f_min:f_max]
-            tracks[track].append(frequency)
-    
-    frequency_mean = np.mean(tracks["mv_0"], axis=0)
-    frequency_std = np.std(tracks["mv_0"], axis=0)
-    for t,track in tracks.iteritems():
-        tracks[t] = [(freq-frequency_mean)/frequency_std for freq in track]
+                frequency[c, :] = np.abs(np.fft.fft(ecog[c,-500:]) ** 2)[f_min/2:f_max/2] -np.abs(np.fft.fft(ecog[c,-1500:-1000]) ** 2)[f_min/2:f_max/2]
+            #if track=="r_arm_1" and np.mean(frequency[47])>3:
+            #    print file
+                #plt.plot(np.transpose(ecog[44:50,-3000:]))
+                #plt.show()
+            if np.median(np.sum(frequency, axis=1)) >100 or np.median(np.sum(frequency,axis=1)) <-100 or np.max(abs(np.sum(frequency,axis=1)))>1000:
+                print file
+                #print np.sum(frequency, axis=1)
+                #plt.plot(np.transpose(ecog))
+                #plt.show()
+            else:
+                tracks[track].append(frequency)
+        displaymat /= float(len(glob.glob(ecog_loc + "/%s/*.npy" % track)))
+
+        for c in range(64):
+            for freq in xrange(displaymat.shape[1]):
+                mean = np.mean(displaymat[c,freq])
+                std = np.std(displaymat[c,freq])
+                displaymat[c, freq,:] -= mean
+                displaymat[c, freq, :] /=std
+        plt.pcolormesh(t, f, displaymat[7])
+        plt.show()
+        pdb.set_trace()
+        plt.pcolormesh(t, np.arange(64), displaymat[:,30])
+        plt.show()
+        plt.pcolormesh(t, np.arange(64), displaymat[:,80])
+        plt.show()
+        #plt.imshow(np.sum(np.array(tracks[track])[:500], axis=2), clim=(-10,10))
+        #plt.show()
+    #frequency_mean = np.mean(tracks["mv_0"], axis=0)
+    #frequency_std = np.std(tracks["mv_0"], axis=0)
+    #for t,track in tracks.iteritems():
+    #    tracks[t] = [(freq-frequency_mean)/frequency_std for freq in track]
 	
     for track in tracks.iterkeys():
-        significance[track] = scipy.stats.ttest_ind(np.sum(tracks[track], axis=2), np.sum(tracks["mv_0"], axis=2), equal_var=False)
+        #significance[track] = scipy.stats.ttest_ind(np.sum(tracks[track], axis=2), np.sum(tracks["mv_0"], axis=2), equal_var=False)
         tracks_final[track] = np.mean(np.array(tracks[track]), axis=0)
+
     print significance
     return tracks_final, significance
 
---
+++ movement/movement_video_gen.py
@@ -0,0 +1,33 @@
+import glob
+import argparse
+import os
+import pickle
+import numpy as np
+import subprocess
+
+def main(file, vid_name, save_dir):
+    mv_file = pickle.load(open(file))
+    right_arm_mvmt = mv_file[:, 1]
+    for f in range(0, len(mv_file), 2):
+        if np.mean(right_arm_mvmt[f:f+5])>1 and np.all(right_arm_mvmt[f-10:f] >= 0) and np.mean(right_arm_mvmt[f-10:f]) < 0.5:
+            start = f
+        if start > 0 and np.mean(right_arm_mvmt[f:f+5])<0.5 and np.all(right_arm_mvmt[f:f+5] >= 0):
+            end = f
+            savename = "%s/%s_i_i.avi" % (save_dir, vid_name.split("/")[-1].split(".")[0], start, end)
+            subprocess.call(
+                "ffmpeg -i %s -ss %i -c copy -t %i %s" % (vid_name, start / 30, int(round(end - start) / 30.0), savename),
+                shell=True)
+            start = -1
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--mv_dir', required=True, help="Joint movement directory")
+    parser.add_argument('-v', '--vid_dir', required=True, help="video directory")
+    parser.add_argument('-s', '--save_dir', required=True, help="Save directory")
+    args = parser.parse_args()
+
+    for file in sorted(glob.glob(args.mv_dir + "/*.p")[64:]):
+        #pdb.set_trace()
+        sbj_id, day, vid_num, _ = os.path.split(file)[-1].split(".")[0].split("_")
+        vid_name = os.path.join(args.vid_dir, "%s_%s_%s.avi" %( sbj_id, day, vid_num))
+        main(file, vid_name, args.save_dir)
