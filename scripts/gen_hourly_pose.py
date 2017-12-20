4fb2c76632852fe413d7deabb28e44787777a0a7
nancywang1991 <nancywang1991@yahoo.ca>
2017-12-20 14:09:40 -0800

    data share code

--- scripts/gen_hourly_pose.py
+++ scripts/gen_hourly_pose.py
@@ -1,48 +1,55 @@
 import pandas
+
 import numpy as np
 from vid.video_sync.vid_start_end import get_disconnected_times
-from movement.joint_movement_norm import normalize_to_camera
+from movement.joint_movement_norm import normalize_to_camera, numerate_coords
 from datetime import datetime
 from datetime import timedelta
 import cPickle as pickle
 import ezodf
 import pdb
+import os
 
-sbj_id = "cb46fd46"
+sbj_id = "c95c1e82"
 conversion_file = "/home/nancy/Documents/data_release/%s.csv" % sbj_id
 disconnect_times_dir = "/home/nancy/Documents/disconnect_times/"
-orig_pose_dir = "/data/pose/%s/" % sbj_id
+vid_start_ends_dir = "/home/nancy/Documents/vid_start_end/"
+orig_pose_dir = "/data/pose/"
 purge_dir = "/home/nancy/Documents/purge_times/"
 save_dir = "/space/nancy/data_by_day/"
 
 
 def time_to_vid(time, vid_start_ends):
     for s, start_end in enumerate(vid_start_ends):
-        for i in len(start_end["start"]):
+        for i in xrange(len(start_end["start"])):
             if time >= start_end["start"][i] and time <= start_end["end"][i]:
-                return s, i, start_end["end"][i]
-            if time > start_end["end"][i]:
-                return None, None, None
-
+                return s, i, start_end["start"][i], start_end["end"][i]
+            if time < start_end["start"][i]:
+                return None, None, None, None
+    return None, None, None, None
 def print_time(time):
     return "%02i:%02i:%02i.%06i" % (time.hour, time.minute, time.second, time.microsecond)
 
 
 def read_ods(filename, sheet_no=0, header=0):
     tab = ezodf.opendoc(filename=filename).sheets[sheet_no]
-    return pandas.DataFrame({col[header].value:[x.value for x in col[header+1:]]
+    return pandas.DataFrame({col[header].value:[x.plaintext() for x in col[header+1:]]
                          for col in tab.columns()})
 
-def is_purged(cur_vid, frame):
+def is_purged(cur_vid, frame, df):
     sbj_id, day, vid = cur_vid.split("_")
-    df = read_ods("%s/%s/%s_%s.ods" % (purge_dir, sbj_id, sbj_id, day), header=1)
-    rows = df.loc[df["video name" == str(vid)]]
+    if df is None:
+	return False
+    try:
+    	rows = df[df["video name"] == str(int(vid))]
+    except KeyError:
+	return False
     for r in xrange(len(rows)):
-        start = rows["start time"][r]
-        end = rows["end time"][r]
-        time = frame/30
-        if (time > (int(start.split(":")[0])*60 + int(start.split(":")[1]))) and \
-                (time < (int(end.split(":")[0])*60 + int(end.split(":")[1]))):
+        start = rows["start time"].iloc[r]
+        end = rows["end time"].iloc[r]
+        time = frame/30.0
+        if (time > (int(start.split(":")[0])*60 + float(start.split(":")[1]))) and \
+                (time < (int(end.split(":")[0])*60 + float(end.split(":")[1]))):
             return True
     return False
 
@@ -50,12 +57,11 @@ def is_purged(cur_vid, frame):
 
 conversion = pandas.read_csv(conversion_file)
 
-for d in xrange(conversion.shape[0]):
-
+for d in range(conversion.shape[0]):
     start_end_times = [get_disconnected_times("%s/%s.txt" % (disconnect_times_dir, orig_file))[:2]
                        for orig_file in [conversion["file1"][d], conversion["file2"][d], conversion["file3"][d]] if
                        not isinstance(orig_file, float)]
-    vid_start_ends = [pickle.load(open("%s/%s.csv" % (disconnect_times_dir, orig_file)))[:2]
+    vid_start_ends = [pickle.load(open("%s/%s.p" % (vid_start_ends_dir, orig_file)))
                        for orig_file in [conversion["file1"][d], conversion["file2"][d], conversion["file3"][d]] if
                        not isinstance(orig_file, float)]
     final_start_time = datetime.strptime("01-0%i-1000 " % conversion["day"][d] + conversion["start_time"][d],
@@ -67,26 +73,43 @@ for d in xrange(conversion.shape[0]):
                                               '%m/%d/%Y %H:%M:%S:%f')
     final_end_time_orig = datetime.strptime(conversion["date"][d] + " " + conversion["end_time"][d],
                                             '%m/%d/%Y %H:%M:%S:%f')
-    result_file = pandas.DataFrame({"time":[], "missing":[], "purged": [], "head":[], "r_shoulder":[], "l_shoulder":[], "r_elbow":[], "l_elbow":[], "r_wrist":[], "l_wrist":[]})
+    header = ["time", "missing", "purged", "head", "r_shoulder", "l_shoulder", "r_elbow", "l_elbow", "r_wrist", "l_wrist"]
+    result_file = []
+    #result_file = pandas.DataFrame({"time":[], "missing":[], "purged": [], "head":[], "r_shoulder":[], "l_shoulder":[], "r_elbow":[], "l_elbow":[], "r_wrist":[], "l_wrist":[]})
     cur_time = final_start_time_orig
     cur_vid_end = final_start_time_orig - timedelta(seconds=1)
-    pdb.set_trace()
     while cur_time < final_end_time_orig:
-        fnum, vnum, cur_vid_end = time_to_vid(cur_time, vid_start_ends)
+	fnum, vnum, cur_vid_start, cur_vid_end = time_to_vid(cur_time, vid_start_ends)
         while fnum == None:
-            cur_time += timedelta(seconds=1/30.0)
-            result_file.append(pandas.DataFrame([[print_time(cur_time)], [1], [0]] + [[(-1,-1,-1)] for i in xrange(7)]))
-            fnum, vnum, cur_vid_end = time_to_vid(cur_time, vid_start_ends)
-        cur_vid = "%s_%04i" % (conversion["file%i" % fnum+1], vnum)
-        poses = open("%s/%s.txt" % (orig_pose_dir, cur_vid)).readlines()
-        crop_coord = open("%s/poses/%s.txt" % (orig_pose_dir, cur_vid)).readlines()
+            result_file.append(pandas.DataFrame([[print_time(cur_time), 1, 0] + [(-1,-1,-1) for i in xrange(7)]], columns=header))
+            cur_time += timedelta(seconds=0.99975/30.0)
+	    fnum, vnum, cur_vid_start, cur_vid_end = time_to_vid(cur_time, vid_start_ends)
+	cur_vid = "%s_%04i" % (conversion["file%i" % (int(fnum)+1)][d], int(vnum))
+        poses = open("%s/%s/%s.txt" % (orig_pose_dir, conversion["file%i" % (int(fnum)+1)][d], cur_vid)).readlines()
+	crop_coord = np.array([np.array([int(coord) for coord in crop_coord.split(',')]) for crop_coord in  open("%s/crop_coords/%s.txt" % (orig_pose_dir, cur_vid)).readlines()])
+        start_frame = int((cur_time-cur_vid_start).total_seconds()*30*0.99975)
+	print (cur_vid, cur_time, start_frame)
+        sbj_id, day, vid = cur_vid.split("_")
+        if not os.path.exists("%s/%s/%s_%s_purgetimes.ods" % (purge_dir, sbj_id, sbj_id, day)):
+            df = None
+	else:
+	    df = read_ods("%s/%s/%s_%s_purgetimes.ods" % (purge_dir, sbj_id, sbj_id, day))
 
-        for p, pose in enumerate(poses):
-            if not is_purged(cur_vid, p):
-                norm_poses = normalize_to_camera(pose, crop_coord[p])
-                result_file.append(pandas.DataFrame([[print_time(cur_time)], [0], [0]] +
-                                                    [[(norm_pose[0], norm_pose[1], orig_pose[2])] for norm_pose, orig_pose in zip(norm_poses, pose)]))
-            else:
+	for p, pose in enumerate(poses[start_frame:]):
+            if not is_purged(cur_vid, p+start_frame, df):
+                norm_poses = normalize_to_camera(numerate_coords(pose), crop_coord[p])
+                result_file.append(pandas.DataFrame([[print_time(cur_time), 0, 0] +
+                                                    [(norm_pose[0], norm_pose[1], orig_pose[2]) for norm_pose, orig_pose in zip(norm_poses, numerate_coords(pose))]], columns=header))
+	    else:
+		#pdb.set_trace()
                 result_file.append(
-                    pandas.DataFrame([[print_time(cur_time)], [0], [1]] + [[(-1, -1, -1)] for i in xrange(7)]))
+                    pandas.DataFrame([[print_time(cur_time), 0, 1] + [(-1, -1, -1) for i in xrange(7)]], columns=header))
+	    cur_time += timedelta(seconds=0.99975/30.0)
+	fnum2, vnum2, _, _ = time_to_vid(cur_time, vid_start_ends)
+	while cur_time< final_end_time_orig and fnum2 is not None and fnum2==fnum and vnum2 != vnum+1:
+		print "adding"
+		result_file.append(pandas.DataFrame([[print_time(cur_time), 1, 0] + [(-1,-1,-1) for i in xrange(7)]], columns=header))
+		cur_time += timedelta(seconds=0.99975/30.0)
+		fnum2, vnum2, _, _ = time_to_vid(cur_time, vid_start_ends)
+    result_file = pandas.concat(result_file, ignore_index=True)
     result_file.to_csv("%s/%s_day_%i.csv" % (save_dir, sbj_id, conversion["day"][d]))
--- scripts/merge_edf_files.py
+++ scripts/merge_edf_files.py
@@ -2,12 +2,12 @@ import pyedflib
 import pandas
 import numpy as np
 from vid.video_sync.vid_start_end import get_disconnected_times
-from datetime import datetime
+from datetime import datetime, timedelta
 from copy import copy
 import pdb
 import gc
 
-sbj_id = "cb46fd46"
+sbj_id = "c95c1e82"
 conversion_file = "/home/nancy/Documents/data_release/%s.csv" % sbj_id
 disconnect_times_dir = "/home/nancy/Documents/disconnect_times/"
 orig_edf_dir = "/data/decrypted_edf/%s/" % sbj_id
@@ -18,13 +18,12 @@ def get_sample_len(time1, time2):
 
 conversion = pandas.read_csv(conversion_file)
 
-for d in range(1, conversion.shape[0]):
-
+for d in range(conversion.shape[0]):
     orig_files = ["%s/%s.edf" % (orig_edf_dir, orig_file) for orig_file in [conversion["file1"][d],conversion["file2"][d],conversion["file3"][d]] if not isinstance(orig_file, float)]
     start_end_times = [get_disconnected_times("%s/%s.txt" % (disconnect_times_dir, orig_file))[:2]
                        for orig_file in [conversion["file1"][d],conversion["file2"][d],conversion["file3"][d]] if not isinstance(orig_file, float)]
-    final_start_time = datetime.strptime("01-0%i-1000 " % conversion["day"][d] + conversion["start_time"][d], '%m-%d-%Y %H:%M:%S:%f')
-    final_end_time = datetime.strptime("01-0%i-1000 " % conversion["day"][d] + conversion["end_time"][d], '%m-%d-%Y %H:%M:%S:%f')
+    final_start_time = datetime.strptime("01-%02i-1000 " % conversion["day"][d] + conversion["start_time"][d], '%m-%d-%Y %H:%M:%S:%f')
+    final_end_time = datetime.strptime("01-%02i-1000 " % conversion["day"][d] + conversion["end_time"][d], '%m-%d-%Y %H:%M:%S:%f')
 
 
     final_start_time_orig = datetime.strptime(conversion["date"][d] + " " + conversion["start_time"][d], '%m/%d/%Y %H:%M:%S:%f')
@@ -41,13 +40,14 @@ for d in range(1, conversion.shape[0]):
 
     # Set signal header
     edf_out.setSignalHeaders(edf_files[0].getSignalHeaders()[1:n_channels+1])
-
+    for c in xrange(n_channels):
+	edf_out.setSamplefrequency(c, 1000)
     # write data
-    physical_min = edf_files[0].getPhysicalMaximum(1)
+    physical_min = edf_files[0].getPhysicalMinimum(1)
     main_data = []
     gc.collect()
     for channel in range(1, n_channels+1):
-        chan_data = np.array([physical_min]*get_sample_len(final_start_time_orig, start_end_times[0][0]))
+        chan_data = np.array([physical_min]*get_sample_len(final_start_time_orig-timedelta(microseconds=final_start_time_orig.microsecond), start_end_times[0][0]))
         for f, file in enumerate(edf_files):
             print "loading channel %i of file %s" % (channel, orig_files[f])
             if final_start_time_orig > start_end_times[f][0]:
@@ -58,7 +58,7 @@ for d in range(1, conversion.shape[0]):
                 chan_data = np.hstack([chan_data, file.readSignal(channel)])
             if f+1 < len(start_end_times):
                 chan_data = np.hstack([chan_data, np.array([physical_min]*get_sample_len(start_end_times[f][1], start_end_times[f+1][0]))])
-	chan_data = copy(chan_data[:len(chan_data)/3])
+	chan_data = copy(chan_data[:(len(chan_data)/3-len(chan_data)/3%1000)])
 	main_data.append(chan_data)
     
     edf_out.writeSamples(main_data)
@@ -66,7 +66,7 @@ for d in range(1, conversion.shape[0]):
     main_data = []
     gc.collect()
     for channel in range(1, n_channels+1):
-        chan_data = np.array([physical_min]*get_sample_len(final_start_time_orig, start_end_times[0][0]))
+        chan_data = np.array([physical_min]*get_sample_len(final_start_time_orig-timedelta(microseconds=final_start_time_orig.microsecond), start_end_times[0][0]))
         for f, file in enumerate(edf_files):
             print "loading channel %i of file %s" % (channel, orig_files[f])
             if final_start_time_orig > start_end_times[f][0]:
@@ -77,14 +77,14 @@ for d in range(1, conversion.shape[0]):
                 chan_data = np.hstack([chan_data, file.readSignal(channel)])
             if f+1 < len(start_end_times):
                 chan_data = np.hstack([chan_data, np.array([physical_min]*get_sample_len(start_end_times[f][1], start_end_times[f+1][0]))])
-        chan_data = copy(chan_data[len(chan_data)/3:2*len(chan_data)/3])
+        chan_data = copy(chan_data[(len(chan_data)/3-len(chan_data)/3%1000):(2*len(chan_data)/3-2*len(chan_data)/3%1000)])
 	main_data.append(chan_data)
     edf_out.writeSamples(main_data)
 
     main_data = []
     gc.collect()
     for channel in range(1, n_channels+1):
-        chan_data = np.array([physical_min]*get_sample_len(final_start_time_orig, start_end_times[0][0]))
+        chan_data = np.array([physical_min]*get_sample_len(final_start_time_orig-timedelta(microseconds=final_start_time_orig.microsecond), start_end_times[0][0]))
         for f, file in enumerate(edf_files):
             print "loading channel %i of file %s" % (channel, orig_files[f])
             if final_start_time_orig > start_end_times[f][0]:
@@ -95,7 +95,7 @@ for d in range(1, conversion.shape[0]):
                 chan_data = np.hstack([chan_data, file.readSignal(channel)])
             if f+1 < len(start_end_times):
                 chan_data = np.hstack([chan_data, np.array([physical_min]*get_sample_len(start_end_times[f][1], start_end_times[f+1][0]))])
-	chan_data = copy(chan_data[2*len(chan_data)/3:])
+	chan_data = copy(chan_data[(2*len(chan_data)/3-2*len(chan_data)/3%1000):])
         main_data.append(chan_data)
     edf_out.writeSamples(main_data)
 
