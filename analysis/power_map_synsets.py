eceea74a9bbd5380b4f6ff4baaf57ed31cb988a2
nancywang1991 <nancywang1991@yahoo.ca>
2018-04-26 18:56:41 -0700

    misc

--- analysis/power_map_synsets.py
+++ analysis/power_map_synsets.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use("agg")
 import argparse
 import csv
 import glob
@@ -6,66 +8,99 @@ import pdb
 import scipy.stats
 import matplotlib.pyplot as plt
 from scipy import signal
-import pickle
+import cPickle as pickle
 import pyedflib
+from datetime import timedelta
+import copy
+import os
 
+test_maps = []
 def transcript2synsets(transcript, syn_dict):
     synsets = []
-    for word in transcript:
+    for word in transcript.split(" "):
         try:
-            synsets.append(syn_dict[word])
-        except LookupError:
+            synsets += syn_dict[word]
+        except KeyError:
             pass
     return synsets
 
 def ecog_spectrogram(ecog_clip, f_min=10, f_max=200):
-    displaymat = np.zeros(shape=(len(ecog_clip), f_max - f_min))
+    displaymat_tmp = np.zeros(shape=(len(ecog_clip), f_max - f_min))
+    displaymat = np.zeros(shape=(len(ecog_clip), 3))
     freq_ratio = (ecog_clip.shape[1])/1000
     for c in xrange(len(ecog_clip)):
-        displaymat[c, :] = np.log(np.abs(np.fft.fft(ecog_clip) ** 2)[f_min*freq_ratio:f_max*freq_ratio:])
+        displaymat_tmp[c,:] = np.log(np.abs(np.fft.fft(ecog_clip[c]) ** 2)[f_min*freq_ratio:f_max*freq_ratio:freq_ratio]).T
+    displaymat[:,0] = np.mean(displaymat_tmp[:, 20:50], axis=1)
+    displaymat[:,1] = np.mean(displaymat_tmp[:, 65:95], axis=1)
+    displaymat[:,2] = np.mean(displaymat_tmp[:, 125:175], axis=1)
     return displaymat
 
-def ecog_map_single_sentence_multi_channel(ecog_clip, transcript, syn_dict, map_dict, f_min=10, f_max=200):
+def ecog_map_single_sentence_multi_channel(ecog_clip, prev_clip, transcript, syn_dict, map_dict, save_dict, f_min=10, f_max=200):
+    #print transcript
     synsets = transcript2synsets(transcript, syn_dict)
-    spectrogram = ecog_spectrogram(ecog_clip, f_min, f_max)
+    spectrogram = ecog_spectrogram(ecog_clip, f_min, f_max) - ecog_spectrogram(prev_clip[:,-(ecog_clip.shape[1]):], f_min, f_max)
+    #if (spectrogram.max() > 10 or spectrogram.min() < -10):
+    #    return
+    #print spectrogram.max()
+    #print spectrogram.min()
+    save_dict["power"].append(spectrogram)
+    save_dict["synset"].append(synsets)
+    save_dict["transcript"].append(transcript)
     for synset in synsets:
         try:
             map_dict[synset]["map"] += spectrogram
             map_dict[synset]["count"] += 1
         except KeyError:
-            map_dict[synset] = {"map": spectrogram, "count": 1}
+            map_dict[synset] = {"map": copy.copy(spectrogram), "count": 1}
     return
 
 def time_clip2edf_pos(clip_name, vid_start_end, day_start):
-    sbj, day, vid_num, start, end = clip_name.split("/")[-1].split(".")[0].split("_")
-    clip_start = vid_start_end["start"][int(vid_num)] + np.floor(float(start))
-    edf_start = int((clip_start-day_start).get_seconds()*1000)
-    edf_dur = (np.ceil(end)-np.floor(start))
+    sbj, day, vid_num, start, end = clip_name.split("/")[-1][:-4].split("_")
+    clip_start = vid_start_end["start"][int(vid_num)] + timedelta(seconds=np.floor(float(start)))
+    edf_start = int((clip_start-day_start).total_seconds()*1000)
+    edf_dur = int((np.ceil(float(end))-np.floor(float(start)))*1000)
     return edf_start, edf_dur
 
-def save_synset_maps(map_dict, save_dir):
+def save_synset_maps(map_dict, save_dir, channels, f_lo, f_hi):
     for synset, synset_map in map_dict.iteritems():
-        final_map = synset_map["map"]/synset_map["count"]
-        plt.clf()
-        plt.pcolormesh(final_map)
-        plt.title("Log power for synset %s" % synset)
-        plt.xlabel("frequency")
-        plt.ylabel("channel")
-        plt.savefig("%s/%s.png" %(save_dir, synset))
+        if synset_map["count"]>50:
+            final_map = synset_map["map"]/synset_map["count"]
+            plt.clf()
+            X1 = np.tile(range(len(channels)), (final_map.shape[1]+1,1))
+            Y1 = np.tile(range(4), (len(final_map),1)).T
+            plt.pcolormesh(X1, Y1, final_map.T, vmin=-0.5, vmax=0.5)
+            plt.title("Log power change for synset %s" % synset)
+            plt.ylabel("frequency")
+            plt.xlabel("channel")
+            plt.xticks(np.arange(len(channels))[::10], channels[::10])
+            plt.yticks(np.arange(3), ["beta", "gamma", "high gamma"])
+            plt.savefig("%s/%s.png" %(save_dir, synset))
 
 
 def main(ecog_file, syn_dict, transcript, vid_start_end, f_min, f_max, save_dir):
-
     map_dict = {}
+    save_dict = {"power":[], "synset":[], "transcript": []}
     ecog = pyedflib.EdfReader(ecog_file)
-    for line in open(transcript):
-        filename, words = transcript.split(",")
+    good_channels = []
+    for c in xrange(ecog.signals_in_file):
+        test_clip = ecog.readSignal(c, start=200000, n=100)
+        if not all(test_clip == 0) and not all(test_clip[0]==test_clip):
+                good_channels.append(c)
+    for l, line in enumerate(open(transcript).readlines()):
+        if l % 100==0:
+            print l
+        filename, words = line.split(",")
         edf_start, edf_dur = time_clip2edf_pos(filename, pickle.load(open(vid_start_end)), ecog.getStartdatetime())
-        ecog_clip = np.zeros(shape=(len(ecog_file, edf_dur*1000)))
-        for c in xrange(len(ecog)):
-            ecog_clip[c,:] = ecog.readSignal(c, start=edf_start, n=edf_dur)
-        ecog_map_single_sentence_multi_channel(ecog_clip, words, syn_dict, map_dict, f_min=f_min, f_max=f_max)
-    save_synset_maps(map_dict, save_dir)
+        ecog_clip = np.zeros(shape=(len(good_channels), edf_dur))
+        prev_clip = np.zeros(shape=(len(good_channels), 10000))
+        for c, chan in enumerate(good_channels):
+            prev_clip[c,:] = ecog.readSignal(chan, start=edf_start-10000, n=10000)
+            ecog_clip[c,:] = ecog.readSignal(chan, start=edf_start, n=edf_dur) 
+        ecog_map_single_sentence_multi_channel(ecog_clip, prev_clip, words, pickle.load(open(syn_dict)), map_dict, save_dict, f_min=f_min, f_max=f_max)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    pickle.dump(save_dict, open(save_dir + "/power_feature_maps.p", "wb"))
+    save_synset_maps(map_dict, save_dir, good_channels, f_min, f_max)
 
 
 
--- speech/classify_synsets.py
+++ speech/classify_synsets.py
@@ -1,10 +1,11 @@
-import sklearn
+import sklearn.svm
 import cPickle as pickle
 import glob
 import numpy as np
 import itertools
 import pdb
-
+import sklearn.ensemble
+from sklearn.model_selection import cross_val_score
 
 def synset_summary(synset_list):
     synset_dict = {}
@@ -24,61 +25,83 @@ def matches(list1, list2):
     return match
 
 def train_val_test_split(samples, synsets_to_do):
-    data_len = min([value for value in samples.itervalues()])
-    trainX = []
-    trainY = []
-    testX = []
-    testY = []
-    valX = []
-    valY = []
+    data_len = min([len(value) for value in samples.itervalues()])
+    train_len = int(0.8*data_len)
+    val_len = int(0.9*data_len)-int(0.8*data_len)
+    test_len = data_len-int(0.9*data_len)
+    sample_shape = np.ndarray.flatten(samples.values()[0][0]).shape[0]
+    categories = len(synsets_to_do)
+    trainX = np.zeros(shape=(train_len*categories, sample_shape))
+    trainY = np.zeros(train_len*categories)
+    testX = np.zeros(shape=(test_len*categories, sample_shape))
+    testY = np.zeros(test_len*categories)
+    valX = np.zeros(shape=(val_len*categories, sample_shape))
+    valY = np.zeros(val_len*categories)
+    cur_cat = 0
     for synset, value in samples.iteritems():
+        value = np.reshape(value, (len(value), sample_shape))
         ind = np.arange(len(value))
         np.random.shuffle(ind)
         y = synsets_to_do.index(synset)
-        trainX += value[ind[:int(0.8*data_len)]]
-        trainY += [y]*int(0.8*data_len)
-        valX += value[ind[int(0.8*data_len): int(0.9*data_len)]]
-        valY += [y]*(int(0.9*data_len)-int(0.8*data_len))
-        testX += value[ind[int(0.9 * data_len): data_len]]
-        testY += [y] * (data_len - int(0.9 * data_len))
-
+        trainX[cur_cat*train_len:(cur_cat+1)*train_len] = value[ind[:train_len]]
+        trainY[cur_cat*train_len:(cur_cat+1)*train_len] = y
+        valX[cur_cat*val_len:(cur_cat+1)*val_len] = value[ind[int(0.8*data_len): int(0.9*data_len)]]
+        valY[cur_cat*val_len:(cur_cat+1)*val_len] = y
+        testX[cur_cat*test_len:(cur_cat+1)*test_len] = value[ind[int(0.9 * data_len): data_len]]
+        testY[cur_cat*test_len:(cur_cat+1)*test_len] = y
+        cur_cat += 1
     return trainX, trainY, testX, testY, valX, valY
 
-def gen_classification_data(data, synset, synsets_to_do):
+def gen_classification_data(data, data_synset, synsets_to_do):
     samples = {}
     for synset in synsets_to_do:
         samples[synset] = []
     for i in xrange(len(data)):
-        match = matches(synset[i], synsets_to_do)
+        match = matches(data_synset[i], synsets_to_do)
         if len(match) == 1:
             samples[match[0]].append(data[i])
     return samples
 
 def gen_synsets_to_do(synset_dict, cutoff, n_way):
     eligible = [synset for synset, count in synset_dict.iteritems() if (count > cutoff)]
-    return list(itertools.combinations(eligible, n_way))
+    combos = list(itertools.combinations(eligible, n_way))
+    return [synsets for synsets in combos if synsets[0].split(".")[1]==synsets[1].split(".")[1]]
 
 
 
-sbj_id = "cb46fd46"
+sbj_id = "a0f66459"
 data_loc = "/home/nancy/Documents/speech/synset_mapping/"
 
 data = []
 synsets =[]
 for file in glob.glob("%s/%s/*/power_feature_maps.p" % (data_loc, sbj_id)):
+    if file.split("/")[-2] == "cb46fd46_4":
+        continue
     input_data = pickle.load(open(file, "rb"))
     data += input_data["power"]
     synsets += input_data["synset"]
-    synset_dict = synset_summary(synsets)
-    synsets_to_do_list = gen_synsets_to_do(synset_dict, 100, 2)
+valid_ind = [i for i, datum in enumerate(data) if not (np.isnan(datum).any() or np.isinf(datum).any())]
+data = np.array(data)[valid_ind]
+synsets = np.array(synsets)[valid_ind]
+synset_dict = synset_summary(synsets)
+synsets_to_do_list = gen_synsets_to_do(synset_dict, 300, 2)
 
-    for synsets_to_do in synsets_to_do_list:
-        print synsets_to_do
-        samples = gen_classification_data(data, synsets, synsets_to_do)
-        trainX, trainY, testX, testY, valX, valY = train_val_test_split(samples, synsets_to_do)
-        model = sklearn.svm.SVC()
-        model.fit(trainX, trainY)
-        print model.score(testX, testY)
-        pdb.set_trace()
+for synsets_to_do in synsets_to_do_list:
+    samples = gen_classification_data(data, synsets, synsets_to_do)
+    if min([len(value) for value in samples.itervalues()]) < 200:
+        continue
+    trainX, trainY, testX, testY, valX, valY = train_val_test_split(samples, synsets_to_do)
+    model = sklearn.ensemble.RandomForestClassifier()
+    print synsets_to_do
+    print np.mean(cross_val_score(model, trainX, trainY, cv=10))
+    #model.fit(trainX, trainY)
+    #score =  model.score(testX, testY)
+    #if score > 0.55:
+    #    print synsets_to_do
+    #    print score
+    #    print model.score(valX, valY)
+    #    print np.mean(cross_val_score(model, trainX, trainY, cv=10))
+    #    print model.score(trainX, trainY)
+    #pdb.set_trace()
 
 
